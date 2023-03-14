4f2e6c73f31bbdc068315ef850a724be31ca62b6
Kauanny Tenorio <kauanny.silva@sptech.school>
2023-03-13 21:35:14 -0300

    Adicionando foreign key e inserindo dados

--- script_sprint1.sql
+++ script_sprint1.sql
@@ -18,7 +18,8 @@ create table funcionario(
     dtNasc date not null,
     email varchar(100) not null,
     loginFuncionario varchar(50) not null,
-    senhaFuncionario varchar(50) not null
+    senhaFuncionario varchar(50) not null,
+	fkEmpresa int, foreign key(fkEmpresa) references empresa(idEmpresa)
 );
 
 create table caminhao(
@@ -29,18 +30,8 @@ create table caminhao(
 create table sensor(
 	idSensor int primary key auto_increment,
     tipoSensor varchar(50) not null, constraint chkTipoSensor check (tipoSensor in ('Temperatura e Umidade')),
-    statusSensor varchar(50) not null, constraint chkStatusSensor check (statusSensor in ('Ativo', 'Inativo', 'Em manutenção'))
-    -- chave estrangeira do caminhão // local onde o sensor vai estar
-);
-
--- tabela q vai conter os dados da rosa //que podemos ou n usar no banco
-create table flor(
-	idFlor int primary key auto_increment,
-    tipoFlor varchar(50) not null,
-    temperaturaMinIdeal float not null,
-    temperaturaMaxIdeal float not null,
-    umidadeMinIdeal float not null,
-    umidadeMaxIdeal float not null
+    statusSensor varchar(50) not null, constraint chkStatusSensor check (statusSensor in ('Ativo', 'Inativo', 'Em manutenção')),
+    fkCaminhao int, foreign key (fkCaminhao) references caminhao(idCaminhao) 
 );
 
 create table capturaTemperaturaUmidade(
@@ -48,8 +39,8 @@ create table capturaTemperaturaUmidade(
     dtCaptura date not null,
     hrCaptura time not null,
     temperatura float not null,
-    umidade float not null
-    -- chave estrangeira do sensor que capturou
+    umidade float not null,
+    fkSensor int, foreign key (fkSensor) references sensor(idSensor)
 );
 
 
@@ -58,10 +49,10 @@ insert into empresa values
 	(null, 'Delivery Roses', '12.345.678/0001-00', 'DeliveryRoses', '1234');
 
 insert into funcionario values
-	(null, 'Administrador', 'Maria Rosaria da Silva', '123.456.789-01', '1985-06-25', 'mariarosario@hotmail.com', 'MariaRosario', '4321'),
-    (null, 'Entregador', 'José Ferreira', '987.654.321-09', '1980-12-01', 'joseferreira@hotmail.com', 'JoseFerreira', '3412'),
-    (null, 'Entregador', 'Bianca de Souza', '741.852.963-07', '1989-02-10', 'biancasouza@hotmail.com', 'BiancaSouza', '2143'),
-    (null, 'Entregador', 'André Paulo Neto', '369.258.147-03', '1993-05-17', 'andreneto@hotmail.com', 'AndrePaulo', '1423');
+	(null, 'Administrador', 'Maria Rosaria da Silva', '123.456.789-01', '1985-06-25', 'mariarosario@hotmail.com', 'MariaRosario', '4321', 1),
+    (null, 'Entregador', 'José Ferreira', '987.654.321-09', '1980-12-01', 'joseferreira@hotmail.com', 'JoseFerreira', '3412', 1),
+    (null, 'Entregador', 'Bianca de Souza', '741.852.963-07', '1989-02-10', 'biancasouza@hotmail.com', 'BiancaSouza', '2143', 1),
+    (null, 'Entregador', 'André Paulo Neto', '369.258.147-03', '1993-05-17', 'andreneto@hotmail.com', 'AndrePaulo', '1423', 1);
     
 insert into caminhao values
 	(null, 'BRA 2K20'),
@@ -69,24 +60,21 @@ insert into caminhao values
     (null, 'BRA 0S17');
     
 insert into sensor values
-	(null, 'Temperatura e Umidade', 'Ativo'),
-    (null, 'Temperatura e Umidade', 'Ativo'),
-    (null, 'Temperatura e Umidade', 'Inativo');
-    
-insert into flor values 
-	(null, 'Rosa', 1, 7, 70, 75);
+	(null, 'Temperatura e Umidade', 'Ativo', 1),
+    (null, 'Temperatura e Umidade', 'Ativo', 2),
+    (null, 'Temperatura e Umidade', 'Inativo', 3);
     
 insert into capturaTemperaturaUmidade values
-	(null, '2023-03-01', '13:00:00', 5, 73),
-    (null, '2023-03-01', '13:30:00', 5, 73),
-    (null, '2023-03-01', '14:00:00', 5, 72),
-    (null, '2023-03-02', '11:00:00', 4, 70),
-    (null, '2023-03-02', '11:30:00', 4, 70),
-    (null, '2023-03-02', '12:00:00', 20, 65),
-    (null, '2023-03-02', '12:30:00', 19, 65),
-    (null, '2023-03-03', '10:00:00', 7, 75),
-    (null, '2023-03-03', '10:30:00', 7, 75),
-    (null, '2023-03-03', '11:00:00', 6, 77);
+	(null, '2023-03-01', '13:00:00', 5, 73, 2),
+    (null, '2023-03-01', '13:30:00', 5, 73, 2),
+    (null, '2023-03-01', '14:00:00', 5, 72, 2),
+    (null, '2023-03-02', '11:00:00', 4, 70, 1),
+    (null, '2023-03-02', '11:30:00', 4, 70, 1),
+    (null, '2023-03-02', '12:00:00', 20, 65, 1),
+    (null, '2023-03-02', '12:30:00', 19, 65, 1),
+    (null, '2023-03-03', '10:00:00', 7, 75, 3),
+    (null, '2023-03-03', '10:30:00', 7, 75, 3),
+    (null, '2023-03-03', '11:00:00', 6, 77, 3);
 
 
 -- Consulta de dados
@@ -124,6 +112,4 @@ select * from sensor
 
 select * from sensor
 	where statusSensor = 'Inativo';    
-    
-select * from flor;
     
